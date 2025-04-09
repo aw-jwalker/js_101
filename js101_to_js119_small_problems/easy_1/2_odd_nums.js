0c784d8371da47706e8f436ae319f30cbdc08337
aw-jwalker <jwalker@assetwatch.com>
2025-04-09 19:34:32 -0400

    init npm

---
+++ js101_to_js119_small_problems/easy_1/2_odd_nums.js
@@ -0,0 +1,8 @@
+function logOdds() {
+  for (let i = 1; i <= 99; i += 2) {
+    console.log(i);
+  }
+}
+
+
+logOdds();
---
+++ js101_to_js119_small_problems/easy_1/4_room_size.js
@@ -0,0 +1,26 @@
+/* How big is the room?
+ * 
+ * Build a program that asks the user to enter the length and width of a room in meters,
+ * and then logs the area of the room to the console in both square meters and square feet.
+ *
+ * Note: 1 square meter == 10.7639 square feet
+ *
+ * Do not worry about validating the input at this time. Use the readlineSync.prompt method to collect user input.
+ */
+
+let rls = require('readline-sync');
+function prompt(phrase) {
+  return rls.question(phrase)
+}
+
+let sqMetersToFeet = 10.7639;
+let length = prompt('Enter the length of the room in meters: ');
+let width = prompt('Enter the width of the room in meters: ');
+
+let areaMeters = length * width;
+let areaFeet = sqMetersToFeet * areaMeters;
+
+console.log('The area of the room in square meters: ' + areaMeters);
+console.log('The area of the room in square feet: ' + areaFeet);
+
+
