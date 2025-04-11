d185cc99e2752bb5941bcdec11950ae9023b4d7a
aw-jwalker <jwalker@assetwatch.com>
2025-04-11 01:37:32 -0400

    exercises 8-10

---
+++ js101_to_js119_small_problems/easy_1/10_mult_3_and_5.js
@@ -0,0 +1,23 @@
+/* Write a function that computes the sum of all numbers between 1 and some other number, inclusive, that are multiples of 3 or 5. For instance, if the supplied number is 20, the result should be 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + 20).
+
+You may assume that the number passed in is an integer greater than 1.
+ */
+
+let isMultiple = function (number, divisor) {
+  return number % divisor === 0
+}
+
+function multisum (number) {
+  let total = 0;
+
+  for (let i = 1; i <= number; i++) {
+    if (isMultiple(i, 3) || isMultiple(i, 5)) {
+      total += i;
+    }
+  }
+}
+
+multisum(3);       // 3
+multisum(5);       // 8
+multisum(10);      // 33
+multisum(1000);    // 234168
