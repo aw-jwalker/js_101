2ff3f2cbabf936cafdf0a5e476e9360409852dfa
aw-jwalker <jwalker@assetwatch.com>
2025-04-15 16:45:19 -0400

    grinding through practice problems

---
+++ 3_practice_problems/2_easy_1.js
@@ -0,0 +1,91 @@
+function question1() {
+  let numbers = [1, 2, 3];
+  numbers[6] = 5;
+  console.log(numbers);
+}
+
+
+function question2() {
+  // determine if a string ends with "!"
+
+  let str1 = "Come over here!"; // true
+  let str2 = "What's up, Doc?"; // false
+
+  let str1LastChar = str1[str1.length - 1];
+  let str2LastChar = str2[str2.length - 1];
+
+  console.log(`str1 last character is "!": `);
+  console.log(str1LastChar === '!');
+  console.log(`str2 last character is "!": `);
+  console.log(str2LastChar === '!');
+
+  // solution: we can use str.endsWith("!")
+
+}
+
+function question3() {
+  // determine whether the object contains an entry for "Spot"
+
+  let ages = { Herman: 32, Lily: 30, Grandpa: 402, Eddie: 10 };
+  let key = 'Herman';
+  console.log(`the object contains an entry for "${key}": `);
+  console.log(Object.keys(ages).includes(key));
+
+  // we can use ages.hasOwnProperty(key);
+}
+
+function question4() {
+  // print all lower case except first character of sentence
+
+  let munstersDescription = "the Munsters are CREEPY and Spooky.";
+  // => The munsters are creepy and spooky.
+
+  let munstersFixed = munstersDescription.toLowerCase();
+  let munstersCapped = munstersFixed[0].toUpperCase() + munstersFixed.slice(2 - munstersFixed.length - 1);
+  console.log(munstersCapped);
+
+  // solution:
+  // munstersDescription.charAt(0).toUpperCase() +
+  //   munstersDescription.substring(1).toLowerCase();
+}
+
+function question6() {
+  // add some entries to the munster family object
+  let ages = { Herman: 32, Lily: 30, Grandpa: 5843, Eddie: 10 };
+  console.log(ages);
+
+  let additionalAges = { Marilyn: 22, Spot: 237 };
+  Object.assign(ages, additionalAges);
+
+  console.log(ages);
+
+
+}
+
+function question7() {
+  // determine whether a word appears in the sentence
+  let str1 = "Few things in life are as important as house training your pet dinosaur.";
+  let str2 = "Fred and Wilma have a pet dinosaur named Dino.";
+  let word = "Dino";
+  console.log(`str1 has ${word}: ${str1.includes(word)}`);
+  console.log(`str2 has ${word}: ${str2.includes(word)}`);
+}
+
+function question9() {
+  // add an element to the array
+  let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
+  flintstones.push("Dino", "Hoppy");
+  console.log(flintstones);
+}
+
+function question10() {
+  // delete everything house and after
+
+  let advice = "Few things in life are as important as house training your pet dinosaur.";
+  let adviceTrunc = advice.slice(0, advice.indexOf('house'));
+  console.log(adviceTrunc);
+  // Expected return value:
+  // => 'Few things in life are as important as '
+}
+
+question10();
---
+++ 3_practice_problems/3_easy_2.js
@@ -0,0 +1,92 @@
+function question1() {
+  // replace a word with another word in the sentence
+  let advice = "Few things in life are as important as house training your pet dinosaur.";
+  let oldWord = "important"
+  let newWord = "urgent"
+  let newAdvice = advice.replace(oldWord, newWord)
+  console.log(newAdvice)
+}
+
+function question2() {
+  // reverse the array without mutating original array (cant use reverse or sort)
+  let letters = ['a', 'b', 'c', 'd', 'e'];
+  let lettersReversed = [];
+  for (let i = 0; i < letters.length; i++) {
+    lettersReversed[letters.length - i] = letters[i];
+  }
+  lettersReversed.shift();
+  console.log(letters);
+  console.log(lettersReversed);
+
+  // solution: let reversedArray = numbers.slice().reverse();
+  // or: numbers.forEach( (number) => {
+  //        reversedArray.unshift(number)
+  //     })
+}
+
+function question3() {
+  let numbers = [1, 2, 3, 4, 5, 15, 16, 17, 95, 96, 99];
+
+  let number1 = 8;  // false
+  let number2 = 95; // true
+
+  console.log(number1 + ' is in the array: ' + numbers.includes(number1));
+  console.log(number2 + ' is in the array: ' + numbers.includes(number2));
+}
+
+function question4() {
+  // insert "four score and " in two different ways
+  let famousWords = "seven years ago...";
+  let moreFamousWords = "Four score and ";
+  let famousSentence = moreFamousWords + famousWords;
+  console.log(famousSentence);
+  console.log(moreFamousWords.concat(famousWords));
+}
+
+function question5() {
+  let letters = ['a', 'b', 'c', 'd', 'e'];
+  letters.splice(2, 1);
+  console.log(letters)
+}
+
+function question6() {
+  // add values to an array, 2 at a time
+  let flintstones = ['Fred', 'Wilma'];
+  flintstones.push(['Barney', 'Betty'], ['Bambam', 'Pebbles']);
+  let flatstones = flintstones.flat();
+  console.log(flintstones);
+  console.log(flatstones);
+}
+
+function question7() {
+  // create an array of just barney's name and number
+  let flintstones = { Fred: 0, Wilma: 1, Barney: 2, Betty: 3, Bambam: 4, Pebbles: 5 };
+  let barneyArr = Object.entries(flintstones)[2];
+  console.log(Object.entries(flintstones).filter(pair => pair[0] === 'Barney').shift())
+  console.log(barneyArr);
+}
+
+function question8() {
+  // check if these are arrays
+  let numbers = [1, 2, 3, 4]; // true
+  let table = { field1: 1, field2: 2, field3: 3, field4: 4 }; // false
+  console.log(Array.isArray(numbers));
+  console.log(Array.isArray(table));
+}
+
+function question9() {
+  let title = "Flintstone Family Members";
+  const TABLE_WIDTH = 40;
+  let padding = Math.floor((TABLE_WIDTH - title.length) / 2);
+  title.padStart(padding + title.length);
+  console.log(title);
+}
+
+function question10() {
+  // count lowercase 't' chars
+  let statement1 = "The Flintstones Rock!";
+  let statement2 = "Easy come, easy go.";
+  console.log(statement1.split('').filter(char => char === 't').length);
+}
+
+question10();
