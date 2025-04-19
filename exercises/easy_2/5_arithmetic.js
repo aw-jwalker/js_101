95de054ace6a39b9c21377f3172e6076c17fcdfc
aw-jwalker <jwalker@assetwatch.com>
2025-04-19 14:54:23 -0400

    more small exercises

--- exercises/easy_2/5_arithmetic.js
+++ exercises/easy_2/5_arithmetic.js
@@ -2,5 +2,19 @@
 
 import { prompt } from '../../utils/prompt.js';
 
-const num1 = prompt('Enter a positive integer: ');
-const num2 = prompt('Enter another positive integer: ');
+let num1 = parseInt(prompt('Enter a positive integer: '));
+let num2 = parseInt(prompt('Enter another positive integer: '));
+let results = {};
+
+
+function runOperations(num1, num2) {
+  results.add = num1 + num2;
+  results.subtract = num1 - num2;
+  results.product = num1 * num2;
+  results.quotient = num1 / num2;
+  results.remainder = num1 % num2;
+  results.power = num1 ** num2;
+  console.log(results);
+}
+
+runOperations(num1, num2);
--- utils/prompt.js
+++ utils/prompt.js
@@ -1,5 +1,5 @@
 import readline from 'readline-sync';
 
-function prompt(phrase) {
+export function prompt(phrase) {
   return readline.question(phrase);
 }
