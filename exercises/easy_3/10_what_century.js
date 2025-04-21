dc046f9125b01739b4f4f7579c75cffd0d17535f
aw-jwalker <jwalker@assetwatch.com>
2025-04-21 17:05:32 -0400

    finished easy_3/

---
+++ exercises/easy_3/10_what_century.js
@@ -0,0 +1,42 @@
+/* Write a function that takes a year as input and returns the century. The return value should be a string that begins with the century number, and ends with 'st', 'nd', 'rd', or 'th' as appropriate for that number.
+
+New centuries begin in years that end with 01. So, the years 1901 - 2000 comprise the 20th century. */
+
+function century(year) {
+  let centuryNumber = Math.floor((year / 100) + 1);
+
+  if (year % 10 === 0) {
+    centuryNumber = centuryNumber - 1;
+  }
+
+  return String(centuryNumber) + centurySuffix(centuryNumber);
+
+}
+
+function catchWithTh(lastTwo) {
+  return lastTwo === 11 || lastTwo === 12 || lastTwo === 13;
+}
+
+function centurySuffix(centuryNumber) {
+  if (catchWithTh(centuryNumber % 100)) {
+    return 'th';
+  }
+
+  let ones = centuryNumber % 10;
+  switch (ones) {
+    case 1: return 'st';
+    case 2: return 'nd';
+    case 3: return 'rd';
+    default: return 'th';
+  }
+}
+
+console.log(century(2000));        // "20th"
+console.log(century(2001));        // "21st"
+console.log(century(1965));        // "20th"
+console.log(century(256));         // "3rd"
+console.log(century(5));           // "1st"
+console.log(century(10103));       // "102nd"
+console.log(century(1052));        // "11th"
+console.log(century(1127));        // "12th"
+console.log(century(11201));       // "113th"
---
+++ exercises/easy_3/7_double_doubles.js
@@ -0,0 +1,29 @@
+/* A double number is an even-length number whose left-side digits are exactly the same as its right-side digits. For example, 44, 3333, 103103, and 7676 are all double numbers, whereas 444, 334433, and 107 are not.
+
+Write a function that returns the number provided as an argument multiplied by two, unless the argument is a double number, in which case, return the double number as-is. */
+
+function twice(num) {
+  let str = num.toString().split('');
+  let right = str.splice(0, (str.length / 2));
+  let left = str;
+
+  for (let i = 0; i < right.length; i++) {
+    if (left[i] !== right[i]) {
+      return num * 2;
+    } else {
+      continue;
+    }
+  }
+
+  return num;
+
+}
+
+console.log(twice(37));          // 74
+console.log(twice(44));          // 44
+console.log(twice(334433));      // 668866
+console.log(twice(444));         // 888
+console.log(twice(107));         // 214
+console.log(twice(103103));      // 103103
+console.log(twice(3333));        // 3333
+console.log(twice(7676));        // 7676
